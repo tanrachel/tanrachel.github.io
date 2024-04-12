5a1183b41622c45468461078f3931c5862060330
tanrachel823 <tanrachel823@gmail.com>
2024-04-12 11:39:52 -0700

    fix color

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
 
 k.loadSprite("map", "./map.png");
 
-k.setBackground(k.Color.fromHex("#311047"));
+k.setBackground(k.Color.fromHex("#afba5a"));
 
 k.scene("main", async () => {
     const mapData = await (await fetch("./map.json")).json();
